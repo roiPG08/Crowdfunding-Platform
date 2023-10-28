fa5bca1ae2910caa5198bcca21ef7f6cd7389012
Przeyslaw Rabczak <87003789+roiPG08@users.noreply.github.com>
2023-10-28 14:47:13 +0200

    Fixed Lock contract

--- server/scripts/deploy.js
+++ server/scripts/deploy.js
@@ -1,27 +1,28 @@
 const hre = require("hardhat");
+const ethers = require("ethers");
 
 async function main() {
-    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  // const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
-    const hello_world = await HelloWorld.deploy("Hello World!");
-    console.log("Contract deployed to address:", hello_world.target);
+  // const hello_world = await HelloWorld.deploy("Hello World!");
+  // console.log("Contract deployed to address:", hello_world.target);
 
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const unlockTime = currentTimestampInSeconds + 60;
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
 
-  // const lockedAmount = parseEther("0.001", "ether");
+  const lockedAmount = ethers.parseEther("0.001");
 
-  // const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-  //   value: lockedAmount,
-  // });
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
 
-  // await lock.waitForDeployment();
+  await lock.waitForDeployment();
 
-  // console.log(
-  //   `Lock with ${ethers.formatEther(
-  //     lockedAmount
-  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  // );
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
 }
 main().catch((error) => {
   console.error(error);
