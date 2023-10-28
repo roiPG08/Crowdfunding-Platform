93940995ec5b91b519e7eca245b451edb1bdfb04
Przeyslaw Rabczak <87003789+roiPG08@users.noreply.github.com>
2023-10-28 14:31:09 +0200

    Deployed first Hello World contract

--- server/contracts/HelloWorld.sol
+++ server/contracts/HelloWorld.sol
@@ -1,57 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+// Specifies the version of Solidity, using semantic versioning.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
+pragma solidity >=0.7.4;
 
-/** @title Documents. */ 
+// Defines a contract named `HelloWorld`.
+// A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
 contract HelloWorld {
-     
-string defaultName;
-mapping (address => string) public accounts; 
- 
-constructor() {
-    defaultName = 'World';
-}
 
-/** @dev Retrieve Message to Print
-      * @return The Message to Print, Hello, Concatenated with the User Name
-      */ 
-function getMessage() public view returns(string memory){
-    string memory name = bytes(accounts[msg.sender]).length > 0 ? accounts[msg.sender] : defaultName;
-    return concat("Hello, " , name);
-}
- 
-/** @dev Set the Name to Greet 
-      * @param  _name  user name
-      * @return success Returns bool value (True or False) to indicate if save was successful or not
-      */
-function setName(string memory _name) public returns(bool success){
-    require(bytes(_name).length > 0);
-    accounts[msg.sender] = _name;
-    return true;
-}
+   // Declares a state variable `message` of type `string`.
+   // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
+   string public message;
 
-/** @dev Set the Name to Greet 
-      * @param  _base  contains the base value " Hello, "
-      * @param  _value contains the name to append to message to display
-      * @return the concatenated string of _base+_value i.e. Hello, Name
-      */ 
- function concat(string memory _base, string memory _value) internal pure returns (string memory) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
- 
-        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
-        bytes memory _newValue = bytes(_tmpValue);
- 
-        uint i;
-        uint j;
- 
-        for(i=0; i<_baseBytes.length; i++) {
-            _newValue[j++] = _baseBytes[i];
-        }
- 
-        for(i=0; i<_valueBytes.length; i++) {
-            _newValue[j++] = _valueBytes[i];
-        }
-        
-        return string(_newValue);
-    }
+   // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
+   // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+   constructor(string memory initMessage) {
+   
+      // Accepts a string argument `initMessage` and sets the value into the contract's `message` storage variable).
+      message = initMessage;
+   }
+
+   // A public function that accepts a string argument and updates the `message` storage variable.
+   function update(string memory newMessage) public {
+      message = newMessage;
+   }
 }
